cac7ea7ffddad5ed4f0b0cc516a68b6e4b60face
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-11-13 17:54:33 -0300

    adapter implemented

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -34,16 +34,120 @@ export function PrismaAdapter(): Adapter {
         emailVerified: null,
       };
     },
-    async getUserByAccount({ providerAccountId, provider }) {},
-    async updateUser(user) {},
-    async deleteUser(userId) {},
-    async linkAccount(account) {},
-    async unlinkAccount({ providerAccountId, provider }) {},
-    async createSession({ sessionToken, userId, expires }) {},
-    async getSessionAndUser(sessionToken) {},
-    async updateSession({ sessionToken }) {},
-    async deleteSession(sessionToken) {},
-    async createVerificationToken({ identifier, expires, token }) {},
-    async useVerificationToken({ identifier, token }) {},
+    async getUserByAccount({ providerAccountId, provider }) {
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        avatar_url: user.avatar_url!, // will be called after user is created
+        emailVerified: null,
+      };
+    },
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      });
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        avatar_url: prismaUser.avatar_url!, // will be called after user is created
+        emailVerified: null,
+      };
+    },
+
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      });
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken,
+        },
+      });
+      return {
+        sessionToken,
+        userId,
+        expires,
+      };
+    },
+    async getSessionAndUser(sessionToken) {
+      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      });
+      return {
+        session: {
+          userId: session.user_id,
+          sessionToken: session.session_token,
+          expires: session.expires,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,
+          avatar_url: user.avatar_url!, // will be called after user is created
+          emailVerified: null,
+        },
+      };
+    },
+    async updateSession({ sessionToken, expires, userId }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      });
+      return {
+        expires: prismaSession.expires,
+        userId: prismaSession.user_id,
+        sessionToken: prismaSession.session_token,
+      };
+    },
   };
 }
