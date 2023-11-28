e00f18ec73418b5df63c4ae2030eae40863a3704
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2023-11-28 15:44:44 -0300

    calendar implemented

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,21 +8,108 @@ import {
   CalendarTitle,
 } from "./styles";
 import { getWeekDays } from "@/src/utils/get-week-days";
+import { useMemo, useState } from "react";
+import dayjs from "dayjs";
+
+interface CalendarWeek {
+  week: number;
+  days: Array<{
+    date: dayjs.Dayjs;
+    disabled: boolean;
+  }>;
+}
+
+type CalendarWeeks = CalendarWeek[];
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonth = currentDate.subtract(1, "month");
+
+    setCurrentDate(previousMonth);
+  }
+  function handleNextMonth() {
+    const nextMonth = currentDate.add(1, "month");
+
+    setCurrentDate(nextMonth);
+  }
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set("date", i + 1); // date aqui quer dizer o dia, pega o dia
+    });
+
+    const fisrtWeekDay = currentDate.get("day"); // para pegar o dia da semana
+
+    // para pegar os dias do mes passado que faltaram para preencher o calendario
+    const previousMonthFillArray = Array.from({
+      length: fisrtWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, "day");
+      })
+      .reverse();
+
+    const lastDayInCurrentMonth = currentDate.set(
+      "date",
+      currentDate.daysInMonth(),
+    );
+    const lastWeekDay = lastDayInCurrentMonth.get("day");
+    const nextMonthFillArray = Array.from({
+      length: 7 - (lastWeekDay + 1),
+    }).map((_, i) => {
+      return lastDayInCurrentMonth.add(i + 1, "day");
+    });
+    const calendarDays = [
+      ...previousMonthFillArray.map((date) => {
+        return { date, disabled: true };
+      }),
+      ...daysInMonthArray.map((date) => {
+        return { date, disabled: false };
+      }),
+      ...nextMonthFillArray.map((date) => {
+        return { date, disabled: true };
+      }),
+    ];
+    const calendarWeeks = calendarDays.reduce<CalendarWeeks>(
+      (weeks, _, i, original) => {
+        // pegando o primeiro dia de uma nova semana
+        const isNewWeek = i % 7 === 0;
+
+        if (isNewWeek) {
+          weeks.push({
+            week: i / 7 + 1,
+            days: original.slice(i, i + 7),
+          });
+        }
+        return weeks;
+      },
+      [],
+    );
+    return calendarWeeks;
+  }, [currentDate]);
+
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Novembro <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
@@ -36,17 +123,21 @@ export function Calendar() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <CalendarDay>1</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay disabled>2</CalendarDay>
-            </td>
-            <td>
-              <CalendarDay>3</CalendarDay>
-            </td>
-          </tr>
+          {calendarWeeks.map(({ days, week }) => {
+            return (
+              <tr key={week}>
+                {days.map(({ date, disabled }) => {
+                  return (
+                    <td key={date.toString()}>
+                      <CalendarDay disabled={disabled}>
+                        {date.get("date")}
+                      </CalendarDay>
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
         </tbody>
       </CalendarBody>
     </CalendarContainer>
--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,10 +1,29 @@
 import { Text, TextArea, TextInput, Button } from "@ignite-ui/react";
-import { ConfirmForm, FormActions, FormHeader } from "./styles";
+import { ConfirmForm, FormActions, FormError, FormHeader } from "./styles";
 import { CalendarBlank, Clock } from "phosphor-react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const confirmFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "O nome precisa no mínimo conter 3 caracteres" }),
+  email: z.string().email({ message: "Digite um email válido" }),
+  observations: z.string().nullable(),
+});
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>;
 
 export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmFormData>({ resolver: zodResolver(confirmFormSchema) });
+  async function handleConfirmScheduling(data: ConfirmFormData) {}
   return (
-    <ConfirmForm as="form">
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
       <FormHeader>
         <Text>
           <CalendarBlank />
@@ -16,21 +35,35 @@ export function ConfirmStep() {
       </FormHeader>
       <label>
         <Text size="sm">Nome completo</Text>
-        <TextInput crossOrigin={undefined} placeholder="Seu nome" />
+        <TextInput
+          crossOrigin={undefined}
+          placeholder="Seu nome"
+          {...register("name")}
+        />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
       </label>
       <label>
         <Text size="sm">Endereço de email</Text>
-        <TextInput crossOrigin={undefined} placeholder="johndoe@gmail.com" />
+        <TextInput
+          crossOrigin={undefined}
+          placeholder="johndoe@gmail.com"
+          {...register("email")}
+        />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
       </label>
       <label>
         <Text size="sm">Observações</Text>
-        <TextArea />
+        <TextArea {...register("observations")} />
       </label>
       <FormActions>
         <Button type="button" variant="tertiary">
           Cancelar
         </Button>
-        <Button type="submit">Confirmar</Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
       </FormActions>
     </ConfirmForm>
   );
